ff18ab96e3df23150f8f9a3def7d226360890c1e
Your Name <you@example.com>
2022-01-25 09:07:32 +0800

    added more features

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect } from 'react';
 
 import Link from 'next/link';
-
-const categories = [{name: "About", slug: 'about'}, { name: "Donate", slug: 'donate'}]
+import { getCategories } from '../services';
 
 const Header = () => {
-    return (
-        <div className="container mx-auto px-10 mb-8">
-            <div className="border-b w-full inline-block border-blue-400 py-8">
-                <div className="md:float-left block">
-                    <Link href="/">
-                        <span className="cursor-pointer font-blod text-4xl text-white">
-                            MYCAT
-                        </span>
-                    </Link>
-                </div>
-                <div className="hidden md:float-left md:contents">
-                    {categories.map((category) => (
-                        <Link key={category.slug} href={'/category/${category.slug}'}>
-                            <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
-                                {category.name}
-                            </span>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [categories, setCategories] = useState([]);
 
-export default Header
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+    <div className="container px-10 mx-auto mb-8">
+      <div className="inline-block w-full py-8 border-b border-blue-400">
+        <div className="block md:float-left">
+          <Link href="/">
+            <span className="text-4xl font-bold text-white cursor-pointer">MYCAT</span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="mt-2 ml-4 font-semibold text-white align-middle cursor-pointer md:float-right">{category.name}</span></Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import moment from 'moment';
 import Link from 'next/link';
 
@@ -38,7 +39,7 @@ const PostWidget = ({ categories, slug }) => {
                         />
                     </div>
                     <div className="flex-grow ml-4">
-                        <p>
+                        <p className="text-gray-500 font-xs">
                             {moment(post.createdAT).format(`MMM DD, YYYY`)}
                         </p>
                         <Link href={`/post/${post.slug}`} key={post.title} className="text-md"> 
